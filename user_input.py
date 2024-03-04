f65d61f7c5d3864d89c08e4fd08249381ba1580e
eusteve <eusteve906@gmail.com>
2024-03-04 13:12:46 +0300

    Simple python file that takes user input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('Hello user ,please enter your name : ')
+age = int(input(' please enter your age : ')) #converts the users name to integer value 
+location = input(' please enter your   currrent location :  ')
+
+print(f"hello {name}.'\n you are {age} years old. '\n  Youre residence is {location}. ")
